04c30187b8e15d8c3a3d695853f8142822813f88
Gurnoor Kaur <gurnoorkr07@gmail.com>
2021-05-27 22:17:30 +0530

    modified

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +39,9 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log6 = new Log(230,180,80,PI/2);
+    chain = new Chain(bird.body,log6.body);
+
 }
 
 function draw(){
@@ -58,7 +64,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    log6.display();
     bird.display();
+    chain.display();
     platform.display();
+
 }
